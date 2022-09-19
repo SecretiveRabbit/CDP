f732be6be82adb3bdfe57b277fb1fc8ddc3c03cc
SecretiveRabbit <70057193+SecretiveRabbit@users.noreply.github.com>
2022-09-19 03:07:55 +0300

    Update outputs.tf

--- Terraform/Home_Task_1/modules/networking/outputs.tf
+++ Terraform/Home_Task_1/modules/networking/outputs.tf
@@ -1,12 +1,24 @@
-output "Webserver-Public-IP" {
-  value = aws_instance.webserver_public_1.public_ip
+output "SG_Public_1_id" {
+  value = aws_security_group.sg_public.id
 }
-output "Webserver-Public-IP-2" {
-  value = aws_instance.webserver_public_2.public_ip
+output "SG_Private_1_id" {
+  value = aws_security_group.sg_private.id
 }
-output "VPC_1_EC2_Private_IP" {
-  value = aws_instance.webserver_private_1.private_ip
+output "SG_Public_2_id" {
+  value = aws_security_group.sg_public_2.id
 }
-output "VPC_2_EC2_Private_IP" {
-  value = aws_instance.webserver_private_2.private_ip
+output "SG_Private_2_id" {
+  value = aws_security_group.sg_private_2.id
+}
+output "VPC_1_Public_Subnet" {
+  value = aws_subnet.Public_subnet_1.id
+}
+output "VPC_1_Private_Subnet" {
+  value = aws_subnet.Private_subnet_1.id
+}
+output "VPC_2_Public_Subnet" {
+  value = aws_subnet.Public_subnet_2.id
+}
+output "VPC_2_Private_Subnet" {
+  value = aws_subnet.Private_subnet_2.id
 }

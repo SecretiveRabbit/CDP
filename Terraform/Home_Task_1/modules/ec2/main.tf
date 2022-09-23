28c16eb7a9d35c04ccee63702303485797162f84
SecretiveRabbit <70057193+SecretiveRabbit@users.noreply.github.com>
2022-09-23 15:22:03 +0300

    Update main.tf

--- Terraform/Home_Task_1/modules/ec2/main.tf
+++ Terraform/Home_Task_1/modules/ec2/main.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "webserver_public_1" {
   instance_type               = var.instance_type
   key_name                    = var.key_name
   associate_public_ip_address = var.aws_instance_associate_public_address
-  vpc_security_group_ids      = [var.SG_Public_1_id]
-  subnet_id                   = var.VPC_1_Public_Subnet
+  vpc_security_group_ids      = [var.sg_public_1_id]
+  subnet_id                   = var.vpc_1_public_subnet
   user_data                   = file("user_data.sh")
 
   tags = merge(var.general_tags, { Name = "Webserver_Public_1" })
@@ -14,8 +14,8 @@ resource "aws_instance" "webserver_private_1" {
   ami                    = var.instance_ami
   instance_type          = var.instance_type
   key_name               = var.key_name
-  vpc_security_group_ids = [var.SG_Private_1_id]
-  subnet_id              = var.VPC_1_Private_Subnet
+  vpc_security_group_ids = [var.sg_private_1_id]
+  subnet_id              = var.vpc_1_private_subnet
 
   tags = merge({ Name = "Webserver_Private_1" }, var.general_tags)
 }
@@ -25,8 +25,8 @@ resource "aws_instance" "webserver_public_2" {
   instance_type               = var.instance_type
   key_name                    = var.key_name
   associate_public_ip_address = var.aws_instance_associate_public_address
-  vpc_security_group_ids      = [var.SG_Public_2_id]
-  subnet_id                   = var.VPC_2_Public_Subnet
+  vpc_security_group_ids      = [var.sg_public_2_id]
+  subnet_id                   = var.vpc_2_public_subnet
   user_data                   = file("user_data.sh")
 
   tags = merge({ Name = "Webserver_Public_2" }, var.general_tags)
@@ -36,8 +36,8 @@ resource "aws_instance" "webserver_private_2" {
   ami                    = var.instance_ami
   instance_type          = var.instance_type
   key_name               = var.key_name
-  vpc_security_group_ids = [var.SG_Private_2_id]
-  subnet_id              = var.VPC_2_Private_Subnet
+  vpc_security_group_ids = [var.sg_private_2_id]
+  subnet_id              = var.vpc_2_private_subnet
 
   tags = merge({ Name = "Webserver_Private_2" }, var.general_tags)
 }

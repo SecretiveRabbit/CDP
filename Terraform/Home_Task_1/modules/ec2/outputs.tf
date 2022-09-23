dc6f41033aa6c335bc9e2e8bec0da1de2d9e905e
SecretiveRabbit <70057193+SecretiveRabbit@users.noreply.github.com>
2022-09-23 15:22:50 +0300

    Update outputs.tf

--- Terraform/Home_Task_1/modules/ec2/outputs.tf
+++ Terraform/Home_Task_1/modules/ec2/outputs.tf
@@ -1,12 +1,12 @@
-output "Webserver-Public-IP" {
+output "webserver_public_ip" {
   value = aws_instance.webserver_public_1.public_ip
 }
-output "Webserver-Public-IP-2" {
+output "webserver-public-ip-2" {
   value = aws_instance.webserver_public_2.public_ip
 }
-output "VPC_1_EC2_Private_IP" {
+output "vpc_1_ec2_private_ip" {
   value = aws_instance.webserver_private_1.private_ip
 }
-output "VPC_2_EC2_Private_IP" {
+output "vpc_2_ec2_private_ip" {
   value = aws_instance.webserver_private_2.private_ip
 }

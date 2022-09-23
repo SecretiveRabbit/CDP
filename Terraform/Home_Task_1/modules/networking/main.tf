9a51da8982414218f21c937a97d7e2a27af5a849
SecretiveRabbit <70057193+SecretiveRabbit@users.noreply.github.com>
2022-09-23 15:21:09 +0300

    Update main.tf

--- Terraform/Home_Task_1/modules/networking/main.tf
+++ Terraform/Home_Task_1/modules/networking/main.tf
@@ -1,28 +1,28 @@
-resource "aws_vpc" "VPC_1" {
-  cidr_block       = var.VPC_1_cidr_block
-  instance_tenancy = var.VPC_1_instance_tenancy
+resource "aws_vpc" "vpc_1" {
+  cidr_block       = var.vpc_1_cidr_block
+  instance_tenancy = var.vpc_1_instance_tenancy
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.VPC_1.id
+  vpc_id = aws_vpc.vpc_1.id
 }
 
-resource "aws_subnet" "Public_subnet_1" {
-  vpc_id                  = aws_vpc.VPC_1.id
-  cidr_block              = var.Public_subnet_1_cidr_block
-  availability_zone       = var.Public_subnet_1_availability_zone
-  map_public_ip_on_launch = var.Public_subnet_1_map_public_ip_on_launch
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.vpc_1.id
+  cidr_block              = var.public_subnet_1_cidr_block
+  availability_zone       = var.public_subnet_1_availability_zone
+  map_public_ip_on_launch = var.public_subnet_1_map_public_ip_on_launch
 }
 
-resource "aws_subnet" "Private_subnet_1" {
-  vpc_id            = aws_vpc.VPC_1.id
-  cidr_block        = var.Private_subnet_1_cidr_block
-  availability_zone = var.Private_subnet_1_availability_zone
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.vpc_1.id
+  cidr_block        = var.private_subnet_1_cidr_block
+  availability_zone = var.private_subnet_1_availability_zone
 }
 
-resource "aws_nat_gateway" "MyNAT_GTW" {
+resource "aws_nat_gateway" "nat_gtw_1" {
   allocation_id = aws_eip.eip_1.id
-  subnet_id     = aws_subnet.Public_subnet_1.id
+  subnet_id     = aws_subnet.public_subnet_1.id
 
   tags = merge({ Name = "GW_NAT_1" }, var.general_tags)
 
@@ -34,8 +34,8 @@ resource "aws_eip" "eip_1" {
   depends_on = [aws_internet_gateway.igw]
 }
 
-resource "aws_network_acl" "ACL_1" {
-  vpc_id = aws_vpc.VPC_1.id
+resource "aws_network_acl" "acl_1" {
+  vpc_id = aws_vpc.vpc_1.id
 
   ingress {
     protocol   = "tcp"
@@ -59,7 +59,7 @@ resource "aws_network_acl" "ACL_1" {
 
 resource "aws_security_group" "sg_public" {
   name   = "sg_pub_1"
-  vpc_id = aws_vpc.VPC_1.id
+  vpc_id = aws_vpc.vpc_1.id
 
   dynamic "ingress" {
     for_each = var.allow_ports
@@ -81,7 +81,7 @@ resource "aws_security_group" "sg_public" {
 
 resource "aws_security_group" "sg_private" {
   name   = "sg_priv_1"
-  vpc_id = aws_vpc.VPC_1.id
+  vpc_id = aws_vpc.vpc_1.id
 
   ingress {
     protocol    = "-1"
@@ -110,63 +110,63 @@ resource "aws_security_group" "sg_private" {
 }
 
 resource "aws_route" "public_route_1" {
-  route_table_id         = aws_vpc.VPC_1.default_route_table_id
+  route_table_id         = aws_vpc.vpc_1.default_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
-resource "aws_route_table" "PrivateRouteTable" {
-  vpc_id = aws_vpc.VPC_1.id
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc_1.id
 
   route {
     cidr_block     = "10.0.2.0/24"
-    nat_gateway_id = aws_nat_gateway.MyNAT_GTW.id
+    nat_gateway_id = aws_nat_gateway.nat_gtw_1.id
   }
   route {
-    cidr_block                = aws_vpc.VPC_2.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.VPC_1-VPC_2.id
+    cidr_block                = aws_vpc.vpc_2.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.vpc_1_vpc_2.id
   }
   tags = merge({ Name = "MyPrivateRouteTable_1" }, var.general_tags)
 }
 
-resource "aws_route_table_association" "RT_for_public" {
-  subnet_id      = aws_subnet.Public_subnet_1.id
-  route_table_id = aws_vpc.VPC_1.default_route_table_id
+resource "aws_route_table_association" "rt_for_public" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_vpc.vpc_1.default_route_table_id
 }
 
 
-resource "aws_route_table_association" "RT_for_private" {
-  subnet_id      = aws_subnet.Private_subnet_1.id
-  route_table_id = aws_route_table.PrivateRouteTable.id
+resource "aws_route_table_association" "rt_for_private" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table.id
 }
 
 # --------------------------------------------------Network 2--------------------------------------------------
 
-resource "aws_vpc" "VPC_2" {
-  cidr_block       = var.VPC_2_cidr_block # set to 10.1.0.0/16
-  instance_tenancy = var.VPC_2_instance_tenancy
+resource "aws_vpc" "vpc_2" {
+  cidr_block       = var.vpc_2_cidr_block # set to 10.1.0.0/16
+  instance_tenancy = var.vpc_2_instance_tenancy
 }
 
 resource "aws_internet_gateway" "igw_2" {
-  vpc_id = aws_vpc.VPC_2.id
+  vpc_id = aws_vpc.vpc_2.id
 }
 
-resource "aws_subnet" "Public_subnet_2" {
-  vpc_id                  = aws_vpc.VPC_2.id
-  cidr_block              = var.Public_subnet_2_cidr_block
-  availability_zone       = var.Public_subnet_2_availability_zone
-  map_public_ip_on_launch = var.Public_subnet_2_map_public_ip_on_launch
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.vpc_2.id
+  cidr_block              = var.public_subnet_2_cidr_block
+  availability_zone       = var.public_subnet_2_availability_zone
+  map_public_ip_on_launch = var.public_subnet_2_map_public_ip_on_launch
 }
 
-resource "aws_subnet" "Private_subnet_2" {
-  vpc_id            = aws_vpc.VPC_2.id
-  cidr_block        = var.Private_subnet_2_cidr_block
-  availability_zone = var.Private_subnet_2_availability_zone
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.vpc_2.id
+  cidr_block        = var.private_subnet_2_cidr_block
+  availability_zone = var.private_subnet_2_availability_zone
 }
 
-resource "aws_nat_gateway" "MyNAT_GTW_2" {
+resource "aws_nat_gateway" "nat_gtw_2" {
   allocation_id = aws_eip.eip_2.id
-  subnet_id     = aws_subnet.Public_subnet_2.id
+  subnet_id     = aws_subnet.public_subnet_2.id
 
   tags = merge({ Name = "GW_NAT_2" }, var.general_tags)
 
@@ -178,8 +178,8 @@ resource "aws_eip" "eip_2" {
   depends_on = [aws_internet_gateway.igw_2]
 }
 
-resource "aws_network_acl" "ACL_2" {
-  vpc_id = aws_vpc.VPC_2.id
+resource "aws_network_acl" "acl_2" {
+  vpc_id = aws_vpc.vpc_2.id
 
   ingress {
     protocol   = "tcp"
@@ -203,7 +203,7 @@ resource "aws_network_acl" "ACL_2" {
 
 resource "aws_security_group" "sg_public_2" {
   name   = "sg_pub_2"
-  vpc_id = aws_vpc.VPC_2.id
+  vpc_id = aws_vpc.vpc_2.id
 
   dynamic "ingress" {
     for_each = var.allow_ports
@@ -224,7 +224,7 @@ resource "aws_security_group" "sg_public_2" {
 
 resource "aws_security_group" "sg_private_2" {
   name   = "sg_priv_2"
-  vpc_id = aws_vpc.VPC_2.id
+  vpc_id = aws_vpc.vpc_2.id
 
   ingress {
     protocol    = "-1"
@@ -253,46 +253,46 @@ resource "aws_security_group" "sg_private_2" {
 }
 
 resource "aws_route" "public_route_2" {
-  route_table_id         = aws_vpc.VPC_2.default_route_table_id
+  route_table_id         = aws_vpc.vpc_2.default_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw_2.id
 }
 
-resource "aws_route_table" "PrivateRouteTable_2" {
-  vpc_id = aws_vpc.VPC_2.id
+resource "aws_route_table" "private_route_table_2" {
+  vpc_id = aws_vpc.vpc_2.id
 
   route {
     cidr_block     = "10.1.12.0/24"
-    nat_gateway_id = aws_nat_gateway.MyNAT_GTW_2.id
+    nat_gateway_id = aws_nat_gateway.nat_gtw_2.id
   }
 
   route {
-    cidr_block                = aws_vpc.VPC_1.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.VPC_1-VPC_2.id
+    cidr_block                = aws_vpc.vpc_1.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.vpc_1_vpc_2.id
   }
 
   tags = merge({ Name = "MyPrivateRouteTable_2" }, var.general_tags)
 }
 
-resource "aws_route_table_association" "RT_for_public_2" {
-  subnet_id      = aws_subnet.Public_subnet_2.id
-  route_table_id = aws_vpc.VPC_2.default_route_table_id
+resource "aws_route_table_association" "rt_for_public_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_vpc.vpc_2.default_route_table_id
 }
 
-resource "aws_route_table_association" "RT_for_private_2" {
-  subnet_id      = aws_subnet.Private_subnet_2.id
-  route_table_id = aws_route_table.PrivateRouteTable_2.id
+resource "aws_route_table_association" "rt_for_private_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table_2.id
 }
 
 #---------------------------------------VPC Peering------------------------------------------------------
 
-resource "aws_vpc_peering_connection" "VPC_1-VPC_2" {
+resource "aws_vpc_peering_connection" "vpc_1_vpc_2" {
   peer_owner_id = var.vpc_peering_connection_peer_owner_id
-  peer_vpc_id   = aws_vpc.VPC_2.id
-  vpc_id        = aws_vpc.VPC_1.id
+  peer_vpc_id   = aws_vpc.vpc_2.id
+  vpc_id        = aws_vpc.vpc_1.id
   auto_accept   = var.vpc_peering_connection_auto_allow
 }
 resource "aws_vpc_peering_connection_accepter" "peering_accept" {
-  vpc_peering_connection_id = aws_vpc_peering_connection.VPC_1-VPC_2.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_1_vpc_2.id
   auto_accept               = var.aws_vpc_peering_connection_accepter_auto_accept
 }

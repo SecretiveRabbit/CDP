1dba72c4a820786530675ed9590cb81bc2e54233
SecretiveRabbit <70057193+SecretiveRabbit@users.noreply.github.com>
2022-09-23 15:21:43 +0300

    Update outputs.tf

--- Terraform/Home_Task_1/modules/networking/outputs.tf
+++ Terraform/Home_Task_1/modules/networking/outputs.tf
@@ -1,24 +1,24 @@
-output "SG_Public_1_id" {
+output "sg_public_1_id" {
   value = aws_security_group.sg_public.id
 }
-output "SG_Private_1_id" {
+output "sg_private_1_id" {
   value = aws_security_group.sg_private.id
 }
-output "SG_Public_2_id" {
+output "sg_public_2_id" {
   value = aws_security_group.sg_public_2.id
 }
-output "SG_Private_2_id" {
+output "sg_private_2_id" {
   value = aws_security_group.sg_private_2.id
 }
-output "VPC_1_Public_Subnet" {
-  value = aws_subnet.Public_subnet_1.id
+output "vpc_1_public_subnet" {
+  value = aws_subnet.public_subnet_1.id
 }
-output "VPC_1_Private_Subnet" {
-  value = aws_subnet.Private_subnet_1.id
+output "vpc_1_private_subnet" {
+  value = aws_subnet.private_subnet_1.id
 }
-output "VPC_2_Public_Subnet" {
-  value = aws_subnet.Public_subnet_2.id
+output "vpc_2_public_subnet" {
+  value = aws_subnet.public_subnet_2.id
 }
-output "VPC_2_Private_Subnet" {
-  value = aws_subnet.Private_subnet_2.id
+output "vpc_2_private_subnet" {
+  value = aws_subnet.private_subnet_2.id
 }

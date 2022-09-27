ba7cbec18ffb5d632a0fddde47105bc8717b81a3
SecretiveRabbit <70057193+SecretiveRabbit@users.noreply.github.com>
2022-09-27 22:39:09 +0300

    Add files via upload

---
+++ Terraform/Home_Task_2/user_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo cd ec2-user
+sudo yum -y update
+sudo yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+sudo echo "<html><body bgcolor=gray><center><h1>WebServer with IP $myip</h1><br>Build by Terraform using external user data, the user data was launched in `pwd`</center></body></html>" > /var/www/html/index.html
+sudo service httpd start

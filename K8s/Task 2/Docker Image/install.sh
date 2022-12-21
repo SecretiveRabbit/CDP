4f982074f78de364e7197decffd14da0bfa754c4
SecretiveRabbit <70057193+SecretiveRabbit@users.noreply.github.com>
2022-12-21 23:59:55 +0200

    Add files via upload

---
+++ K8s/Task 2/Docker Image/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt update -y; apt install -y default-jdk
+apt update -y ; apt install -y maven
+apt-get update -y ; apt-get install -y git
+git config --global user.name "SecretiveRabbit"
+git config --global user.email "sanya21911@gmail.com"
+
